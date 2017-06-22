225faed0e26497b1c642d7a04f192c383b94fcd9
alexsmartins14 <alexsmartins14@gmail.com>
2017-06-22 12:06:29 -0300

    Add files via upload
    esse é modulo dos usuários

--- modules.js
+++ modules.js
@@ -1,1 +1,10 @@
-angular.module('bloganm', ['ngRoute', 'ui.bootstrap', 'bloganm.users']);
+angular.module('bloganm.users', []);
+
+angular.module('bloganm.users').config(function($routeProvider){
+	$routeProvider
+	.when('users/registre', {
+		controller: 'RegistreUsersController',
+		templateUrl: 'static/modules/users/registre/view.html'
+	})
+
+});
